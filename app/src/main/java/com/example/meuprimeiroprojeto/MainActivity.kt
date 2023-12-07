605f5d08fdbf04dbc789d441d5e00265ede01646
breno <brenocarvalhomachado@gmail.com>
2023-12-06 23:20:46 -0300

    commit

---
+++ app/src/main/java/com/example/meuprimeiroprojeto/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.meuprimeiroprojeto
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnCalcular: Button = findViewById(R.id.btnCalcular)
+        val edtPeso : EditText = findViewById(R.id.edittext_peso)
+        val edtAltura : EditText = findViewById(R.id.edittexte_altura)
+        val tvResultado : TextView = findViewById(R.id.textoMsg)
+
+
+        btnCalcular.setOnClickListener {
+
+            val alturaStr = edtAltura.text.toString()
+            val pesoStr = edtPeso.text.toString()
+
+            if(alturaStr.isNotEmpty() && pesoStr.isNotEmpty()) {
+
+
+                val altura: Float = alturaStr.toFloat()
+                val alturafinal: Float = altura * altura
+                val peso: Float = pesoStr.toFloat()
+                val result = peso / alturafinal
+                println(result)
+
+                tvResultado.text = result.toString()
+
+                val intent = Intent(this, ResultActivity::class.java)
+                    .apply {
+                        putExtra("EXTRA_RESULT", result)
+                    }
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Preencher todos os campos", Toast.LENGTH_LONG).show()
+            }
+
+        }
+
+    }
+}
+
+
+
+
